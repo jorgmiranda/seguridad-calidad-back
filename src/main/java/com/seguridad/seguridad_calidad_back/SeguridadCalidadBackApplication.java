a1b8e1231ba28d3ad22f989f2d6e767c725d1916
jorge-miranda <jorge.miranda@mysolutions.cl>
2024-10-26 15:20:23 -0300

    Primer commit

---
+++ src/main/java/com/seguridad/seguridad_calidad_back/SeguridadCalidadBackApplication.java
@@ -0,0 +1,13 @@
+package com.seguridad.seguridad_calidad_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadCalidadBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguridadCalidadBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seguridad/seguridad_calidad_back/SeguridadCalidadBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seguridad.seguridad_calidad_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguridadCalidadBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
